9461b7dc2c08aff469c78b23be884e44af6a06c3
hardevkoder <haroldvaldes@yahoo.com>
2023-01-26 17:37:04 -0500

    Evio retorno del suiche al recargar pagina

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-
+//Capturo el label, Checkbox y el body
 let modo = document.getElementById("lbl");
+const checkbox = document.getElementById("switch");
 let body = document.body;
 
-modo.addEventListener("click", () => {
+// Acciones al presionar el suiche Deslizante
+modo.addEventListener("click", function() {
+  // Guardo el estado del body (Dark/light)
   let val = body.classList.toggle("dark");
   localStorage.setItem('modo',val);
+
+  // Guardo el estado del checkbox para evitar devolucion
+  localStorage.setItem("checkboxState", checkbox.checked);
+
 });
 
-let valor = localStorage.getItem('modo');
+// Recupero los estados del body y checkbox
+const valor = localStorage.getItem('modo');
+const checkboxState = localStorage.getItem("checkboxState");
 
+// Asigno los ultimos estados al body y al suiche
 if(valor=='true'){
   body.classList.add('dark');
+  checkbox.checked = true;
 }else{
   body.classList.remove('dark');
+  checkbox.checked = false;
 }
